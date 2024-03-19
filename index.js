64f0d944580df712be9ecdf5738f5d3a31ea0e74
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-19 14:40:02 +0300

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    let sent = `This Saturday, I want to ${activity}!`
+    return sent
+}
+function mondayWork(activity = "go to the office") {
+    let sent = `This Monday, I will ${activity}.`
+    return sent
+}
+function wrapAdjective(style= "*"){
+    return function (msg= "a hard worker") {
+        return `You are ${style}${msg}${style}!`
+    }
+}
+function wrapAdjective(style = "|") {
+    return function(msg="a dedicated programmer") {
+        return `You are ${style}${msg}${style}!`
+    }
+}
